583219ddcb83e0bc71f9fba5e2d15bbd35ac15bc
AngelGab006 <angelbeltrea.20@gmail.com>
2023-10-25 16:32:51 -0400

    update pointer fuctionality

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # countriesProject
+### this is a project made for a better understanding of promises and fetch an api.
+### i'm glad for all the corrections, suggestions and tips that you could give me.
+#### thank's for reading 
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ const input = document.getElementById('countriesInput');
 const imgContainer = document.getElementById('imgContainer');
 const nameContainer = document.getElementById('nameContainer');
 const favicon = document.querySelector("link[rel*='icon']");
+const filter = document.querySelector('.filter');
+const ubicacion = document.getElementById('ubicacion');
 let countries = [];
 const url = 'https://restcountries.com/v2/all';
 fetch(url)
@@ -28,12 +30,39 @@ function searcher(value) {
             .catch(err=>rejected(`ocurrio un error inesperado ${err}`))
     });
 };
+function pathUbication(value) {
+    return new Promise((resolve,rejected)=>{
+        try {
+            let elemento = document.querySelector(`path[title='${value}']`);
+            if(elemento == null)throw new Error('este pais no esta en el mapa');
+            console.log(elemento)
+            let pathD= elemento.getAttribute('d').split(' ');
+            resolve(pathD[1]);
+        } catch (error) {
+            rejected(`ha ocurrido un error: ${error}`)
+        }
+    })
+};
+function X10(path) {
+    let pathDX10 =  path.split(',');
+    pathDX10[0] = Number(pathDX10[0]) * 10
+    pathDX10[1] = Number(pathDX10[1]) * 10
+    return pathDX10.join(',')
+}
+function cambiarUbicacion(path) {
+    let arrUbicacion = ubicacion.getAttribute('d').split(' ')
+    arrUbicacion[1]= path;
+    arrUbicacion = arrUbicacion.join(' ');
+    ubicacion.setAttribute('d',arrUbicacion);
+}
 async function search(value) {
     imgContainer.innerHTML = `<img class="flags" src="../assets/img/cargando.webp" class="img">`;
     favicon.href = '../assets/img/cargando.webp';
     nameContainer.innerHTML = `<label id="name" for="img">cargando...</label>`
     const img = await searcher(value);
-    console.log(img);
+    let pathD = await pathUbication(value);
+    pathD = X10(pathD);
+    cambiarUbicacion(pathD);
     imgContainer.innerHTML = `<img class="flags" src="${img}" class="img">`;
     favicon.href = img;
     nameContainer.innerHTML = `<label id="name" for="img">${value}</label>`;
